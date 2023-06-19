800a6ecc3f16fb273d0d528f4df907f17da49953
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-20 02:50:47 +0300

    feat: add base structure to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<div>
+				<Link href="https://github.com/RimidalU" target="_blank">
+					RimidalU &copy; 2023
+				</Link>
+			</div>
+		</footer>
+	);
 }
