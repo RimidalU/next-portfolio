f298660849b051c8b88564169084742dad9263b1
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-20 02:35:42 +0300

    feat: add base structure to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,49 @@
+import Link from "next/link";
+
+const links = [
+	{
+		id: 1,
+		title: "Home",
+		url: "/",
+	},
+	{
+		id: 2,
+		title: "Portfolio",
+		url: "/portfolio",
+	},
+	{
+		id: 3,
+		title: "Blog",
+		url: "/blog",
+	},
+	{
+		id: 4,
+		title: "About",
+		url: "/about",
+	},
+	{
+		id: 5,
+		title: "Contacts",
+		url: "/contacts",
+	},
+	{
+		id: 6,
+		title: "Dashboard",
+		url: "/dashboard",
+	},
+];
+
 export function Header() {
-    return(<div>Header</div>)
+	return (
+		<header>
+			<Link href={"/"}>RimidalU</Link>
+			<nav>
+				{links.map((link) => (
+					<Link key={link.id} href={link.url}>
+						{link.title}
+					</Link>
+				))}
+			</nav>
+		</header>
+	);
 }
