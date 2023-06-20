3215b473f216f2b3dde27ad427e7c525fd3c5751
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-20 21:10:31 +0300

    feat: add header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -35,14 +35,17 @@ const links = [
 
 export function Header() {
 	return (
-		<header>
-			<Link href={"/"}>RimidalU</Link>
-			<nav>
+		<header className="h-24 flex justify-between items-center">
+			<Link href={"/"} className="font-bold text-xl">
+				RimidalU
+			</Link>
+			<nav className="flex items-center gap-5">
 				{links.map((link) => (
-					<Link key={link.id} href={link.url}>
+					<Link key={link.id} href={link.url} className="">
 						{link.title}
 					</Link>
 				))}
+				<button className="p-2 bg-emerald-800 rounded-md">LogOut</button>
 			</nav>
 		</header>
 	);
