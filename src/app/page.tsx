9738c12b2880a9649f48a0023f290fa41154c56f
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-21 02:25:24 +0300

    feat: implement home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-	return <div className="">Home page</div>;
+	return (
+		<section className="flex flex-row gap-6 items-center min-h-full">
+			<article
+				className="flex flex-col flex-1 gap-10 bg-clip-text 
+				text-transparent bg-gradient-to-b from-emerald-800 to-neutral-300"
+			>
+				<h2 className="text-7xl font-extrabold">Hey!</h2>
+				<h1 className="text-6xl font-extrabold">I&apos;m Uladzimir.</h1>
+				<p className="text-5xl font-extrabold">I&apos;m a fullstack developer.</p>
+				<Link href={"/"} className="inline-block w-fit border-2 py-2 px-4 rounded-lg border-orange-700 hover:rounded-2xl hover:border-emerald-800 ">
+					Resume
+				</Link>
+			</article>
+			<aside className="">
+				<Image src="/workplace.png" alt="My workplace" className="dark:invert" width={500} height={500} />
+			</aside>
+		</section>
+	);
 }
