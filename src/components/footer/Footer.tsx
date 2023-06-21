8677c7fa210346c683a9fb5d2da4c81ee3f547f5
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-21 17:38:17 +0300

    refactor: minor refactor footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,9 @@ export function Footer() {
 	return (
 		<footer className="h-14 mx-auto flex items-center gap-4">
 			<Link href="https://github.com/RimidalU" target="_blank">
+				<span>&copy;Copyright 2023 </span>
 				RimidalU
 			</Link>
-			<span>&copy; 2023</span>
 		</footer>
 	);
 }
