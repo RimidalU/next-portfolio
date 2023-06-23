2e057d032d1636c5a665221321656924faa7eec8
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-23 15:43:09 +0300

    feat: add postal stamp

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -10,8 +10,16 @@ export default function Contacts() {
 				<article className="flex-1 flex flex-col gap-5 justify-center">
 					<h2 className="text-3xl bold capitalize">Contact Links</h2>
 				</article>
-				<article className="flex-1 flex flex-col gap-5 justify-center">
+				<article className="flex-1 flex flex-col gap-5 justify-evenly">
 					<ContactForm />
+					<div className="w-full h-32 relative">
+						<Image
+							src="/stamp.png"
+							alt="Santa's postal stamp"
+							className="dark:invert object-contain animate-translateY"
+							fill={true}
+						/>
+					</div>
 				</article>
 			</section>
 		</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			keyframes: {
+				translateY: {
+					"from": { transform: "translateY(-15px)" },
+					"to": { transform: "translateY(0) scale(0.97)" },
+				},
+			},
+			animation: {
+				translateY: "translateY 3s ease-in-out infinite alternate",
+			},
+		},
+	},
+	plugins: [],
+};
