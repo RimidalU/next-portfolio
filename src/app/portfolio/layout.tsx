59cce1ab579985ba8127ec7272a1bd977c49e5b6
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-23 17:30:42 +0300

    feat: implement base portfolio layout

---
+++ src/app/portfolio/layout.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+	title: "Uladzimir Stankevich - Portfolio. | My projects",
+};
+
+export default function PortfolioLayout({ children }: { children: React.ReactNode }) {    //add navigation??
+	return (
+		<div>
+			<header>
+				<h1 className="">My projects</h1>  
+			</header>
+			{children}
+		</div>
+	);
+}
