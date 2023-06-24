f4a3682e5fa2d33fe31f84b78035335c7ed17911
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-25 00:25:37 +0300

    feat: implement category page

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,3 +1,18 @@
-export default function Category() {
-	return <div className="">Category Page</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+import { projects } from "./projects";
+import { ProjectCard } from "@/components/projectCard/ProjectCard";
+
+export default function Category({ params }: any) {
+	return (
+		<section className="flex flex-col flex-1 gap-5 px-3 items-center">
+			<h2 className="title-text">{params.category}</h2>
+			<ul className="w-full h-42 overflow-y-scroll no-scrollbar">
+				{projects.map((project) => {
+					return <ProjectCard data={project} key={project.id} />;
+				})}
+			</ul>
+		</section>
+	);
 }
---
+++ src/app/portfolio/[category]/projects.ts
@@ -0,0 +1,66 @@
+export const projects = [
+	{
+		id: 1,
+		name: "Excel to Word Converter.",
+		description:
+			"Converter from excel to word with intermediate state to json. Allows you to get data from an excel-file in the form required by the user using a custom template.",
+		img: "excel-to-word_converter",
+		github: "https://github.com/RimidalU/excel-to-word_converter",
+		liveDemo: "https://excel-to-word-converter.netlify.app/",
+		tools: ["HTML", "TypeScript", "Sass", "JSON", "Yarn", "Vite"],
+		status: "in progress",
+	},
+	{
+		id: 2,
+		name: "Notes text editor with tags.",
+		description:
+			"SPA in React, TypeScript and Sass for quick notes. Create, edit, view and delete notes. While editing a note, user can create tags using the '#' symbol. When editing a note, all words that match the tags are highlighted.",
+		img: "notes_text_editor_with_tags",
+		github: "https://github.com/RimidalU/notes-text-editor_with-tags",
+		liveDemo: "https://euphonious-heliotrope-5b2c49.netlify.app/",
+		tools: ["React", "TypeScript", "HTML", "Sass", "JSON", "Yarn", "Vite"],
+		status: "in progress",
+	},
+	{
+		id: 3,
+		name: "Budget Table.",
+		description: "Table of construction and erection work for budget app.",
+		img: "table_of_construction_and_erection_work",
+		github: "https://github.com/RimidalU/table_of_construction_and_erection_work",
+		liveDemo: "https://table-of-construction-and-erection-work-kdf7uyyt8-rimidalu.vercel.app/",
+		tools: ["React", "Redux", "TypeScript", "HTML", "Sass", "Ky", "Yarn", "Vite"],
+		status: "in progress",
+	},
+	{
+		id: 4,
+		name: "Contact-form in PHP and MySQL.",
+		description:
+			"PHP. A form that adds to the database with notification of messages by e-mail + a table that displays all messages in the database with pagination.",
+		img: "php_contact_form",
+		github: "https://github.com/RimidalU/php-contact_form",
+		liveDemo: "http://php-contact-form.epizy.com/",
+		tools: ["PHP", "HTML", "Bootstrap", "PHPMailer"],
+		status: "in progress",
+	},
+	{
+		id: 5,
+		name: "Travel Landing.",
+		description: "My family's travel landing page.",
+		img: "travel_landing",
+		github: "https://github.com/RimidalU/travel_landing",
+		liveDemo: "https://spiffy-donut-4c3548.netlify.app/",
+		tools: ["JavaScript", "HTML", "Bootstrap"],
+		status: "in progress",
+	},
+	{
+		id: 6,
+		name: "React binary tree.",
+		description:
+			"Binary tree with random number generator. At the user's request, a new random value is embedded in the tree.",
+		img: "react_binary_tree",
+		github: "https://github.com/RimidalU/react_binary_tree",
+		liveDemo: "https://react-binary-tree-k7vywh0ab-rimidalu.vercel.app/",
+		tools: ["React", "TypeScript", "HTML", "CSS", "D3Tree", "Yarn", "Vite"],
+		status: "in progress",
+	},
+];
