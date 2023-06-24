b6e4094c31ada9bbc4e6cc2cd4464df5b9c4d726
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-24 19:35:28 +0300

    refactor: add bg text class to portfolio items

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 const categories = [
 	{
 		id: 1,
-		name: "Web",
+		name: "web",
 		url: "/web",
 		img: "/web.jpg",
 	},
 	{
 		id: 2,
-		name: "Apps",
+		name: "apps",
 		url: "/apps",
 		img: "/apps.jpg",
 	},
@@ -32,7 +32,7 @@ export default function Portfolio() {
 								style={{ backgroundImage: `url(${category.img})` }}
 								className="w-64 h-96 border-2 py-2 px-4 rounded-lg border-emerald-800 hover:rounded-2xl hover:border-orange-700  bg-cover not: grayscale hover:filter-none"
 							></div>
-							<span className="absolute bottom-4 left-4 p-2 italic text-lg bg-gradient-to-r from-emerald-800 from-75% pr-16 capitalize">
+							<span className="paint-stroke">
 								{category.name}
 							</span>
 						</Link>
