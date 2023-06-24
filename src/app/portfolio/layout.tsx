c67b9ab8e9a27505fd1dd71cf976910b293757c1
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-24 16:03:47 +0300

    feat: implement partfolio page

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -2,13 +2,13 @@ export const metadata = {
 	title: "Uladzimir Stankevich - Portfolio. | My projects",
 };
 
-export default function PortfolioLayout({ children }: { children: React.ReactNode }) {    //add navigation??
+export default function PortfolioLayout({ children }: { children: React.ReactNode }) {	//add navigation??
 	return (
-		<div>
-			<header>
-				<h1 className="title-text">My projects</h1>  
+		<>
+			<header className="">
+				<h1 className="title-text">My projects</h1>
 			</header>
 			{children}
-		</div>
+		</>
 	);
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,44 @@
+import Link from "next/link";
+
+const categories = [
+	{
+		id: 1,
+		name: "Web",
+		url: "/web",
+		img: "/web.jpg",
+	},
+	{
+		id: 2,
+		name: "Apps",
+		url: "/apps",
+		img: "/apps.jpg",
+	},
+	{
+		id: 3,
+		name: "implementations",
+		url: "/implementations",
+		img: "/implementations.jpg",
+	},
+];
 export default function Portfolio() {
-	return <div className="">Portfolio Page</div>;
+	return (
+		<section className="flex flex-col flex-1 gap-5 h-fit">
+			<h1 className="title-text">Choose a category</h1>
+			<section className="flex flex-wrap gap-10 justify-center text-justify h-fit">
+				{categories.map((category) => {
+					return (
+						<Link href={`portfolio${category.url}`} key={category.id} className="relative">
+							<div
+								style={{ backgroundImage: `url(${category.img})` }}
+								className="w-64 h-96 border-2 py-2 px-4 rounded-lg border-emerald-800 hover:rounded-2xl hover:border-orange-700  bg-cover not: grayscale hover:filter-none"
+							></div>
+							<span className="absolute bottom-4 left-4 p-2 italic text-lg bg-gradient-to-r from-emerald-800 from-75% pr-16 capitalize">
+								{category.name}
+							</span>
+						</Link>
+					);
+				})}
+			</section>
+		</section>
+	);
 }
