8d5b6417c54335c3aab392e41e0789c3f8d5dff7
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-26 02:53:00 +0300

    feat: implement fetch projects data from js object in file

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
-import Link from "next/link";
+import { notFound } from "next/navigation";
 
-import { projects } from "./projects";
 import { ProjectCard } from "@/components/projectCard/ProjectCard";
 
+import { projects } from "./projects";
+
+const getData = (category: "web" | "apps" | "implementations") => {
+	return projects[category];
+};
+
 export default function Category({ params }: any) {
+	const projectsInCategory = getData(params.category);
+
+	if (!projectsInCategory) {
+		notFound();
+	}
 	return (
 		<section className="flex flex-col flex-1 gap-5 px-3 items-center">
 			<h2 className="title-text">{params.category}</h2>
 			<ul className="w-full h-42 overflow-y-scroll no-scrollbar">
-				{projects.map((project) => {
+				{projectsInCategory.map((project) => {
 					return <ProjectCard data={project} key={project.id} />;
 				})}
 			</ul>
