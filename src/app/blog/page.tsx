0413499c9a15f7d91277290aae34b2042067816a
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-25 18:25:13 +0300

    feat: implement posts page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,52 @@
+import Link from "next/link";
+
+import { ShortPostCard } from "@/components/shortPostCard/ShortPostCard";
+
 export default function Blog() {
-	return <div className="">Blog Page</div>;
+	const posts = [
+		{
+			id: 1,
+			title: "Post 1",
+			subtitle: "Post about Lorem",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde.",
+			author: "Me",
+			date: "2023",
+			image: "/apps.jpg",
+		},
+		{
+			id: 2,
+			title: "Post 2",
+			// subtitle: "My CV",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde.",
+			author: "Me",
+			date: "2023",
+			image: "/web.jpg",
+		},
+		{
+			id: 2,
+			title: "Cats",
+			subtitle: "Cats vs dogs",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde.",
+			author: "Me",
+			date: "2023",
+			image: "/etretat.jpg",
+		},
+	];
+	return (
+		<section className="flex flex-col flex-1 gap-5 px-3 items-center">
+			<h2 className="title-text">thoughts and useful things that I would like to keep.</h2>
+			<ul className="w-full h-42 overflow-y-scroll no-scrollbar">
+				{posts.map((post) => {
+					return (
+						<Link key={post.id} href={`blog/${post.title}`}>
+							<ShortPostCard data={post} />
+						</Link>
+					);
+				})}
+			</ul>
+		</section>
+	);
 }
---
+++ src/components/shortPostCard/ShortPostCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export function ShortPostCard({ data }: any) {
+	return (
+		<section className="flex gap-15 text-justify mb-6">
+			<article className="flex-1 flex flex-col gap-5 p-5">
+				<div className="h-36 min-h-full relative">
+					<Image
+						src="/implementations.jpg"
+						alt="Project"
+						className="object-cover border-4 rounded-lg border-emerald-800"
+						fill={true}
+					/>
+					<h2 className="paint-stroke">{data.title}</h2>
+				</div>
+			</article>
+			<article className="flex-1 flex flex-col gap-3 p-2">
+				<h2 className="title-text">{data.subtitle || data.title}</h2>
+				<p className="line-clamp-4">{data.description}</p>
+			</article>
+		</section>
+	);
+}
