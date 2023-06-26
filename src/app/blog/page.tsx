e32bcb5bb05b1b49fa10ec4f1ca67eb88ed022ab
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-26 17:52:16 +0300

    feat: implement fetching all posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,47 +1,29 @@
 import Link from "next/link";
 
 import { ShortPostCard } from "@/components/shortPostCard/ShortPostCard";
+import { IPost } from "@/types";
 
-export default function Blog() {
-	const posts = [
-		{
-			id: 1,
-			title: "Post 1",
-			subtitle: "Post about Lorem",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde.",
-			author: "Me",
-			date: "2023",
-			image: "/apps.jpg",
-		},
-		{
-			id: 2,
-			title: "Post 2",
-			// subtitle: "My CV",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde.",
-			author: "Me",
-			date: "2023",
-			image: "/web.jpg",
-		},
-		{
-			id: 3,
-			title: "Cats",
-			subtitle: "Cats vs dogs",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dignissimos veritatis illum exercitationem in enim similique sed, magni iure unde.",
-			author: "Me",
-			date: "2023",
-			image: "/etretat.jpg",
-		},
-	];
+async function getData() {
+	
+	const res = await fetch(`${process.env.HOST_URL}api/posts`, { cache: "no-store" });
+	console.log(res);
+
+	if (!res.ok) {
+		throw new Error("Filed to fetch data");
+	}
+	return res.json();
+}
+
+export default async function Blog() {
+	const posts: IPost[] = await getData()
+
 	return (
 		<section className="flex flex-col flex-1 gap-5 px-3 items-center">
 			<h2 className="title-text">thoughts and useful things that I would like to keep.</h2>
 			<ul className="w-full h-42 overflow-y-scroll no-scrollbar">
 				{posts.map((post) => {
 					return (
-						<Link key={post.id} href={`blog/${post.title}`}>
+						<Link key={post._id} href={`blog/${post.title}`}>
 							<ShortPostCard data={post} />
 						</Link>
 					);
