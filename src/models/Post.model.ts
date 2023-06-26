e771ddccbfd505723f180e518999d44b9b69cf0d
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-26 17:07:57 +0300

    feat: implement post  model

---
+++ src/models/Post.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema(
+	{
+		title: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		subtitle: {
+			type: String,
+		},
+		description: {
+			type: String,
+			require: true,
+		},
+		author: {
+			// TODO: add link to User entity
+			type: String,
+			require: true,
+		},
+		image: {
+			//TODO implement img loading
+			type: String,
+			require: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.models["Post"] || mongoose.model("Post", PostSchema);  //https://stackoverflow.com/questions/59851947/now-mongoose-cannot-overwrite-model-once-compiled
