e5ad0a8610e83075ecbbda0d952359b118a8347c
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-27 20:17:49 +0300

    feat: implement CredentialsProvider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
-import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+import { compare } from "bcrypt";
+
+import { connect } from "@/utils/mongodb";
+import User from "@/models/User.model";
 
 const handler = NextAuth({
 	providers: [
@@ -7,7 +12,42 @@ const handler = NextAuth({
 			clientId: process.env.GOOGLE_CLIENT_ID || "",
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
 		}),
+		CredentialsProvider({
+			id: "credentials",
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "email", placeholder: "j@smith.com" },
+				password: { label: "Password", type: "password", placeholder: "*****" },
+			},
+			async authorize(credentials) {
+				await connect();
+
+				try {
+					const user = await User.findOne({
+						email: credentials?.email,
+					});
+
+					if (user) {
+						const isPasswordCorrect = await compare(credentials?.password!, user.password);
+
+						if (isPasswordCorrect) {
+							return user;
+						} else {
+							throw new Error("User not found!");
+						}
+					} else {
+						throw new Error("User not found!");
+					}
+				} catch (error) {
+					throw new Error(`Database error${error}`);
+				}
+			},
+		}),
 	],
+	pages: {
+		signIn: "/dashboard/login",
+		error: "/dashboard/login",
+	},
 });
 
 export { handler as GET, handler as POST };
