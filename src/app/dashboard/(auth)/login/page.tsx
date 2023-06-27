198ecaf5ae5ac5511a0fac1a8622f4e8938cfa44
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-27 15:29:34 +0300

    feat: implement login  page with google auth

--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/button/Button";
+import Link from "next/link";
+
 export default function Login() {
-	return <div className="">Login Page</div>;
+	return (
+			<section className="flex flex-col flex-1 gap-5 h-full">
+				<h1 className="title-text">Remind me who you are...</h1>
+				<Button text="Login with Google" variant='loginGoogle'></Button>
+				<h3 className="">
+					If you have already registered in the system, you will log in with your account.
+				</h3>
+				<Link href={"/dashboard/register"}>
+					<span className="">register here</span>
+				</Link>
+			</section>
+	);
 }
