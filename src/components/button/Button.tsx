e9fb05a5275b50a48487761599259a0d01558b46
RimidalU <stankevichuuladimiru@gmail.com>
2023-06-27 12:39:41 +0300

    refactor: add props type to Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,26 +2,23 @@
 
 import { signIn, signOut } from "next-auth/react";
 
-const handler = (variant: 'logOut' | 'loginGoogle' | 'none') => {
+const handler = (variant: "logOut" | "loginGoogle" | "none") => {
 	const action = {
 		logOut: () => signOut(),
-		loginGoogle: () => signIn('google'),
-		none: () =>undefined
-	}
+		loginGoogle: () => signIn("google"),
+		none: () => undefined,
+	};
 
-	return action[variant]()
-}
+	return action[variant]();
+};
 
-export function Button({
-	text,
-	type = "button",
-	variant
-}: {
+type Props = {
 	text: string;
 	type?: "button" | "submit" | "reset" | undefined;
-	variant: 'logOut' | 'loginGoogle' | 'none'
-}) {
+	variant: "logOut" | "loginGoogle" | "none";
+};
 
+export function Button({ text, type = "button", variant }: Props) {
 	return (
 		<button type={type} onClick={() => handler(variant)}>
 			{text}
