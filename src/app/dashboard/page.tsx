3da8a00c5a68f2edb512c37c26fe583b05b3dcaa
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-02 23:54:53 +0300

    refactor: add auth widdleware

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Dashboard() {
-	const session = useSession();
-	const router = useRouter();
-
-	useEffect(() => {
-		//https://stackoverflow.com/questions/62336340/cannot-update-a-component-while-rendering-a-different-component-warning
-		if (session.status === "unauthenticated") {
-			router.push("/dashboard/login");
-		}
-	}, [router, session.status]);
-
-	if (session.status === "loading") {
-		return <p> Loading ... </p>;
-	}
-
-	if (session.status === "authenticated") {
-		return <div className=""> Dashboard Page </div>;
-	}
+	return <div className=""> Dashboard Page </div>;
 }
