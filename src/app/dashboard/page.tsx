69c4e34eb310d52f77372e3f4ae3dad375f464cd
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-03 16:36:58 +0300

    feat: implement dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import { NewPostForm } from "@/components/newPostForm/NewPostForm";
+import { PostSet } from "@/components/postSet/PostSet";
+
 export default function Dashboard() {
-	return <div className=""> Dashboard Page </div>;
+	return (
+		<section className="flex flex-col flex-1 gap-5 h-full">
+			<section className="flex gap-10 text-justify h-full">
+				<article className="flex-1 flex flex-col gap-14 justify-center title-text">
+					<PostSet />
+				</article>
+				<article className="flex-1 flex flex-col gap-5 justify-evenly">
+					<NewPostForm />
+				</article>
+			</section>
+		</section>
+	);
 }
