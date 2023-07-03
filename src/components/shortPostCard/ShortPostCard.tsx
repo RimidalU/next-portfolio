1052400667ca2d44eef39870179e886ad78c4ca7
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-04 01:19:09 +0300

    fix: fix empty data.image field

--- src/components/shortPostCard/ShortPostCard.tsx
+++ src/components/shortPostCard/ShortPostCard.tsx
@@ -8,15 +8,18 @@ export function ShortPostCard({ data }: Props) {
 	return (
 		<section className="flex text-justify">
 			<article className="flex-1 flex flex-col gap-5 p-5">
-				<div className="h-36 min-h-full relative">
-					<Image
-						src={data.image!}
-						alt="Project"
-						className="object-cover border-4 rounded-lg border-emerald-800"
-						fill={true}
-					/>
-				</div>
+				{data.image && (
+					<div className="h-36 min-h-full relative">
+						<Image
+							src={data.image}
+							alt="Project"
+							className="object-cover border-4 rounded-lg border-emerald-800"
+							fill={true}
+						/>
+					</div>
+				)}
 			</article>
+
 			<article className="flex-1 flex flex-col gap-3 p-2">
 				<h2 className="text-xl">{data.subtitle || data.title}</h2>
 				<p className="line-clamp-4 text-base">{data.description}</p>
