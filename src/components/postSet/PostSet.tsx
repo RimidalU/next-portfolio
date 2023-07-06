365d06478a49b93150af47eb0c09f674e83b8ca9
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-07 00:26:35 +0300

    feat: implement remove post metod

--- src/components/postSet/PostSet.tsx
+++ src/components/postSet/PostSet.tsx
@@ -5,19 +5,38 @@ import useSWR, { Fetcher } from "swr";
 
 import { ShortPostCard } from "../shortPostCard/ShortPostCard";
 import { IPost } from "@/types";
+import { useState } from "react";
 
 export function PostSet() {
+	const [errorMessage, setErrorMessage] = useState(false);
+
 	const session = useSession();
 
 	const fetcher: Fetcher<IPost[], string> = (...args) => fetch(...args).then((res) => res.json());
 
-	const { data, mutate, error, isLoading } = useSWR(
+	const { data, isLoading } = useSWR(
 		`/api/posts?email=${session?.data?.user?.email}`,
 		fetcher
 	);
 
+	const { mutate } = useSWR(`/api/posts?email=${session?.data?.user?.email}`);
+	
+	const handleRemove = async (id: string) => {
+		try {
+			await fetch(`/api/posts/${id}`, {
+				method: "DELETE",
+			});
+
+			mutate();
+		} catch (error) {
+			setErrorMessage(true);
+		}
+	};
+
 	return (
 		<article className="flex-1 flex flex-col gap-14 title-text overflow-y-auto no-scrollbar ">
+			{errorMessage && <span className="text-orange-700">Something went wrong!</span>}
+
 			{isLoading && <p> Loading ... </p>}
 
 			{!data?.length && !isLoading && (
@@ -30,7 +49,10 @@ export function PostSet() {
 			{data &&
 				data.reverse().map((post) => (
 					<div key={post._id} className="relative border-4 rounded-lg border-emerald-800">
-						<span className="text-red-500 font-black text-5xl absolute right-0 bottom-16 cursor-pointer ">
+						<span
+							className="text-red-500 font-black text-5xl absolute right-0 bottom-16 cursor-pointer "
+							onClick={() => handleRemove(post._id)}
+						>
 							X
 						</span>
 						<ShortPostCard data={post} />
