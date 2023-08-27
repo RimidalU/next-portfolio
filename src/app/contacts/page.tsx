efae667feb6e7fda52d1aa24ad0b4bd51f962932
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-28 01:23:16 +0300

    refactor: refactor contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,55 +1,44 @@
-import { ContactForm } from "@/components/contactForm/ContactForm";
-import Image from "next/image";
 import Link from "next/link";
 
-const contactLinks = [
-	{
-		id: 1,
-		name: "email me",
-		url: "mailto:stankevichuuladimiru@gmail.com?subject=Fast%20message%20from%20my%20CV.",
-	},
-	{
-		id: 2,
-		name: "linkedin",
-		url: "https://www.linkedin.com/in/uladzimir-stankevich/",
-	},
-	{
-		id: 3,
-		name: "GitHab",
-		url: "https://github.com/RimidalU",
-	},
-];
+import { ContactForm } from "@/components/contactForm/ContactForm";
 
-export const metadata = {
-	title: "Uladzimir Stankevich - Portfolio. | Contacts",
-};
+import { CONTACT_LINKS, METADATA, TContactsLink } from "@/constants";
 
-export default function Contacts() {
+export const metadata = METADATA.CONTACTS;
+
+export default function ContactsPage() {
+	return (
+		<ContactsPageLayout
+			contactForm={<ContactForm />}
+			links={CONTACT_LINKS.map((link) => {
+				return <ContactLink key={link.id} link={link} />;
+			})}
+		/>
+	);
+}
+
+function ContactsPageLayout({
+	links,
+	contactForm,
+}: {
+	links: JSX.Element[];
+	contactForm: JSX.Element;
+}) {
 	return (
 		<section className="flex flex-col flex-1 gap-5 h-full">
 			<h1 className="title-text">From here you can contact me.</h1>
 			<section className="flex gap-10 text-justify h-full">
-				<article className="flex-1 flex flex-col gap-14 justify-center title-text">
-					{contactLinks.map((link) => {
-						return (
-							<Link key={link.id} href={link.url} target="blank" className="capitalize text-center">
-								{link.name}
-							</Link>
-						);
-					})}
-				</article>
-				<article className="flex-1 flex flex-col gap-5 justify-evenly">
-					<ContactForm />
-					<div className="w-full h-32 relative">
-						<Image
-							src="/stamp.png"
-							alt="Santa's postal stamp"
-							className="dark:invert object-contain animate-translateY"
-							fill={true}
-						/>
-					</div>
-				</article>
+				<article className="flex-1 flex flex-col gap-14 justify-center title-text">{links}</article>
+				<article className="flex-1 flex flex-col gap-5 justify-evenly">{contactForm}</article>
 			</section>
 		</section>
+	);
+}
+
+function ContactLink({ link }: { link: TContactsLink }) {
+	return (
+		<Link href={link.url} target="blank" className="capitalize text-center">
+			{link.name}
+		</Link>
 	);
 }
