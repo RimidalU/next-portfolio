96c6119f66b4910a9c2c2dbda419ec7768e8e0bc
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-28 16:36:22 +0300

    feat: implement routing in project types

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -16,7 +16,6 @@ export default function Category({ params }: any) {
 	}
 	return (
 		<section className="flex flex-col flex-1 gap-5 px-3 items-center">
-			<h2 className="title-text">{params.category}</h2>
 			<ul className="w-full h-42 overflow-y-scroll no-scrollbar">
 				{projectsInCategory.map((project) => {
 					return <ProjectCard data={project} key={project.id} />;
